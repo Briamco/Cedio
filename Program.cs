6c892a3bc5e3f0ade51cac0495fa1ba00539444e
Briamco <crossinerobriamco@gmail.com>
2025-07-24 22:10:19 -0400

    add: program title

--- Program.cs
+++ Program.cs
@@ -6,12 +6,30 @@ internal class Program
 {
   private static void Main(string[] args)
   {
+    Console.Clear();
+    StyleConsole.WriteLine(@"
+      _____        ______        _____    ____         _____    
+  ___|\    \   ___|\     \   ___|\    \  |    |   ____|\    \   
+ /    /\    \ |     \     \ |    |\    \ |    |  /     /\    \  
+|    |  |    ||     ,_____/||    | |    ||    | /     /  \    \ 
+|    |  |____||     \--'\_|/|    | |    ||    ||     |    |    |
+|    |   ____ |     /___/|  |    | |    ||    ||     |    |    |
+|    |  |    ||     \____|\ |    | |    ||    ||\     \  /    /|
+|\ ___\/    /||____ '     /||____|/____/||____|| \_____\/____/ |
+| |   /____/ ||    /_____/ ||    /    | ||    | \ |    ||    | /
+ \|___|    | /|____|     | /|____|____|/ |____|  \|____||____|/ 
+   \( |____|/   \( |_____|/   \(    )/     \(       \(    )/    
+    '   )/       '    )/       '    '       '        '    '     
+        '             '", ConsoleColor.Blue);
+    InputHelper.Continuar();
+
     if (!OperatingSystem.IsWindows())
     {
       StyleConsole.Error("No se podran reproducir los sonidos en este sistema operativo.");
     }
 
     IdeaData.Load();
+    AnimationHelper.LoadingAnimation("Cargando");
 
     ScreenMain.Screen();
   }
